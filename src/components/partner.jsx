318539abf17942c3b39fd8c969691c5617615179
4uphwang <4uphwang@gmail.com>
2023-03-05 17:40:29 +0900

    near_logo

--- src/components/partner.jsx
+++ src/components/partner.jsx
@@ -1,5 +1,5 @@
 import Creditscend from "../assets/img/CreditScend.png";
-
+import NearKorea from "../assets/img/nearkr-white.svg";
 
 const Partner = () => {
     return(
@@ -7,6 +7,7 @@ const Partner = () => {
             <div className="Partner_h1">Our Partner</div>
             <div className="Partners">
                 <img src={Creditscend} alt = "Creditscend" className="Partner"/>
+                <img src={NearKorea} alt = "NearKorea" className="Partner"/>
             </div>
         </section>
     )
