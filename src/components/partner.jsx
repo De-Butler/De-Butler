d312378176a36b7b36a9407f2dc93ca4fc353d58
4uphwang <4uphwang@gmail.com>
2023-05-18 20:22:58 +0900

    Image Change

--- src/components/partner.jsx
+++ src/components/partner.jsx
@@ -1,15 +1,17 @@
 import Creditscend from "../assets/img/CreditScend.png";
 import NearKorea from "../assets/img/nearkr-white.svg";
+import YourD from "../assets/img/YourD-white.png";
 
 const Partner = () => {
     return(
         <section className="PartnerSection">
             <div className="Partner_h1">Our Partner</div>
             <div className="Partners">
-                <img src={Creditscend} alt = "Creditscend" className="Partner"/>
+                <img src={YourD} alt="YourD" className="Partner"/>
                 <a href="https://nearkorea.org">
                     <img src={NearKorea} alt = "NearKorea" className="Partner" />
                 </a>
+                <img src={Creditscend} alt = "Creditscend" className="Partner"/>
             </div>
         </section>
     )
