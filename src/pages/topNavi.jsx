23c101426a6c135d041dd45a507154439c615171
4uphwang <4uphwang@gmail.com>
2023-06-20 16:12:38 +0900

    logo change

--- src/pages/topNavi.jsx
+++ src/pages/topNavi.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { HashLink } from 'react-router-hash-link';
+import DeButlerLogo from '../assets/img/De-Butler-white.png';
 
 const topNavi = () => {
     return (
         <div className="Navibox">
-            <HashLink smooth to="/#Home" className="Title">De-Butler</HashLink>
+            <HashLink smooth to="/#Home" className="Title"><img src={DeButlerLogo} style={{height:"60px"}} /></HashLink>
+
             <div className="Menu">
                 <HashLink smooth to="/#About" className='Link' >학회 소개</HashLink>
                 <HashLink smooth to="/#Activity" className='Link'>학회 활동</HashLink>
